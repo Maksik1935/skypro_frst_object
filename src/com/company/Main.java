165c1bb0601ea4e9a31dcf205d944156cf10ba93
m-kac <m-kachalov@mail.ru>
2022-04-24 23:52:40 +0300

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Author scherbenevVadim = new Author("Scherbenev Vadim");
+        Author repinAndrey = new Author("Repin Andrey");
+        Book valentinka = new Book("Valentinka", 1931, scherbenevVadim);
+        Book bananaKokosa = new Book("Banana Kokosa", 39564, repinAndrey);
+
+        bananaKokosa.setYear(2056);
     }
 }
